9e7d880799db45bf7b06ac9a5ac90852158b82ae
Bruno Correia <correiadesigner7@gmail.com>
2026-08-16 16:35:13 -0300

    Redireciona rota raiz para a documentacao do Swagger

--- core/urls.py
+++ core/urls.py
@@ -2,17 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# Importações do drf-spectacular
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
+    # Redireciona a raiz (/) para a documentacao do Swagger
+    path('', RedirectView.as_view(url='/api/docs/swagger/', permanent=False)),
+    
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
     
     # Endpoints do Swagger
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), # Gera o arquivo de esquema (YAML/JSON)
-    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), # Interface Swagger
-    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'), # Interface alternativa Redoc
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 if settings.DEBUG:
